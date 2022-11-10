71bc0695c25a0caeea1c5aacb6b8ecca585343e2
Programming Bees <hasanakash799@gmail.com>
2022-11-10 14:15:37 +0600

    model creation for category and porduct

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category,Product
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id","name","parent"]
+    list_display_links = ["id","parent"]
+
+admin.site.register(Category,CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display= ['id','name','category','price','is_stock','preview_des']
+    list_display_links = ["id","name"]
+admin.site.register(Product,ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(_("Category Name"), max_length=150,blank=False, null=False)
+    image = models.ImageField(_("Category Logo"), upload_to="category/",blank=True, null=True)
+    parent = models.ForeignKey("self",related_name='children',on_delete=models.CASCADE,blank=True,null=True,verbose_name=_("children"))
+    created = models.DateTimeField(_("Creation date"), auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ['-created',]
+        verbose_name_plural = 'Categories'
+
+class Product(models.Model):
+    name = models.CharField(_("Product Name"), max_length=150)
+    category = models.ForeignKey(Category, verbose_name=_("category"),related_name='category', on_delete=models.CASCADE)
+    preview_des = models.CharField(_("Short Description"), max_length=500,blank=True,null=True)
+    description = RichTextField(_("Description"))
+    image = models.ImageField(_("Product Image"), upload_to="products/")
+    price = models.FloatField(_("Product Price"))
+    old_price = models.FloatField(_("Product Old Price"))
+    is_stock = models.BooleanField(_("Poduct available or not"),default=True)
+    created = models.DateTimeField(_("Date"), auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ['-created',]
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
