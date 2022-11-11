2816c5c03b2aa544e4d0fa7ad325dce60d201c57
Programming Bees <hasanakash799@gmail.com>
2022-11-12 01:33:25 +0600

    feta: adding dynalic category for globlal page

---
+++ store/templatetags/category.py
@@ -0,0 +1,13 @@
+from django import template
+from store.models import Category
+
+register = template.Library()
+
+
+@register.filter
+def category(user):
+    if user.is_authenticated:
+        cate = Category.objects.filter(parent=None)
+        return cate
+    else:
+        return f"You are not register user"
